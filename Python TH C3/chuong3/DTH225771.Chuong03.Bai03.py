078f4c7901179aacc348aab2b7742bf58a1dc5b2
Lê Nhật Thiên <lenhatthien198@gmail.com>
2024-09-22 14:56:57 +0700

    DTH225771.Chuong03.Bai03.py

--- Python TH C3/chuong3/DTH225771.Chuong03.Bai03.py
+++ Python TH C3/chuong3/DTH225771.Chuong03.Bai03.py
@@ -1,8 +1,26 @@
-print("Chương trình tính điểm trung bình")
-toan, ly, hoa=eval(input("Nhập điểm toán, lý, hóa:"))
-print("Điểm toán: ",toan)
-print("Điểm lý: ",ly)
-print("Điểm hóa: ",hoa)
-dtb=(toan+ly+hoa)/3
-print("Điểm trung bình: ",dtb)
-print("Điểm làm tròn: ",round(dtb,2))
+from math import sqrt
+print("Chương trình giải phương trình bậc 2")
+a=float(input("Nhập a: "))
+b=float(input("Nhập b: "))
+c=float(input("Nhập c: "))
+if a == 0:
+    if b == 0 and c ==0:
+        print("Phương trình vô số nghiệm")
+    elif b == 0 and c!=0:
+        print("Phương trình vô nghiệm")
+    else:
+        x=-c/b
+        print("x= ",x)
+else:
+    delta=b**2-4*a*c
+    if delta < 0:
+        print("Phương trình vô nghiệm")
+    elif delta == 0:
+        x=-b/(2*a)
+        print("Phương trình có nghiệm kép x1=x2= ",x)
+    else:
+        x1=(-b-sqrt(delta))/(2*a)
+        x2=(-b+sqrt(delta))/(2*a)
+        print("Phương trình có 2 nghiệm: ")
+        print("x1= ", x1)
+        print("x2= ", x2)
