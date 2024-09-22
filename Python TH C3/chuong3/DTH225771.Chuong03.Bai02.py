597edd326f05100d2e6b576b8c196927da355b24
Lê Nhật Thiên <lenhatthien198@gmail.com>
2024-09-22 14:07:56 +0700

    DTH225771.Chuong03.Bai02.py

--- Python TH C3/chuong3/DTH225771.Chuong03.Bai02.py
+++ Python TH C3/chuong3/DTH225771.Chuong03.Bai02.py
@@ -1,5 +1,14 @@
-t=int(input("Nhập số giây: "))
-hour=(t//3600)%24
-minute=(t%3600)//60
-second=(t%3600)%60
-print(hour,":",minute,":",second)
+print("Chương trình đếm số ngày trong tháng")
+month=int(input("Mời bạn nhập vào 1 tháng: \n"))
+if month in (1,3,5,7,8,10,12):
+  print("Tháng", month, "có 31 ngày")
+elif month in (4,6,9,11):
+  print("Tháng", month, "có 30 ngày")
+elif month==2:
+  year=int(input("Mời bạn nhập thêm năm: "))
+  if(year % 4 == 0 and year % 100 !=0) or year % 400 == 0:
+    print("Tháng", month, "có 29 ngày")
+  else:
+    print("Tháng", month, "có 28 ngày")  
+else:
+  print("Tháng", month, "không hợp lệ")
