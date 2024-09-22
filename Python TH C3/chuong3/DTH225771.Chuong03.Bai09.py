dc958f337b3a38fd4c9cdbd5b70364a88751eb9a
Lê Nhật Thiên <lenhatthien198@gmail.com>
2024-09-22 21:20:18 +0700

    Update DTH225771.Chuong03.Bai09.py

--- Python TH C3/chuong3/DTH225771.Chuong03.Bai09.py
+++ Python TH C3/chuong3/DTH225771.Chuong03.Bai09.py
@@ -1,55 +1,14 @@
-i1=2
-i2=5
-i3=-3
-d1=2.0
-d2=5.0
-d3=-0.5
-a=i1 + (i2 * i3)
-b=i1 * (i2 + i3)
-c=i1 / (i2 + i3)
-d=i1 // (i2 + i3)
-e=i1 / i2 + i3
-f=i1 //i2 + i3
-g=3 + 4 + 5/3
-h=3 + 4 + 5//3
-i=(3 + 4 + 5)/3
-j=(3 + 4 + 5)//3
-k=d1 + (d2 * d3)
-l=d1 + d2 * d3
-m=d1 / d2 - d3
-n=d1 / (d2 - d3)
-o=d1 + d2 + d3 / 3
-p=(d1 + d2 + d3) / 3
-q=d1 + d2 + (d3 / 3)
-r=3 * (d1 + d2) * (d1 - d3)
-print("(a) il + (i2 * i3) = ",a)
-print("(b) il * (i2 + i3) = ",b)
-print("(c) il / (i2 + i3) = ",c)
-print("(d) il // (i2 + i3) = ",d)
-print("(e) il / i2 + i3 = ",e)
-print("(f) il // i2 + i3 = ",f)
-print("(g) 3 + 4 + 5/3 = ",g)
-print("(h) 3 + 4 +5//3 = ",h)
-print("(i) (3 + 4 + 5) / 3 = ",i)
-print("(j) (3 + 4 + 5) //3 = ",j)
-print("(k) d1 + (d2 * d3) = ",k)
-print("(l) d1 + d2 * d3 = ",l)
-print("(m) d1 / d2 - d3 = ",m)
-print("(n) d1 / (d2 - d3) = ",n)
-print("(o) d1 + d2 + d3 / 3 = ",o)
-print("(p) (d1 + d2 + d3) / 3 = ",p)
-print("(q) d1 + d2 + (d3 / 3) = ",q)
-print("(r) 3 * (d1 + d2) * (d1 - d3) = ",r)
-
-
-
-
-
-
-
-
-
-
-
-
-
+def xac_dinh_quy(thang):
+    if 1 <= thang <= 3:
+        return "Quý 1"
+    elif 4 <= thang <= 6:
+        return "Quý 2"
+    elif 7 <= thang <= 9:
+        return "Quý 3"
+    elif 10 <= thang <=12:
+        return "Quý 4"
+    else:
+        print("Tháng không hợp lệ")
+    
+thang=int(input("Nhập một tháng (1-12): "))
+print(f"Tháng {thang} thuộc {xac_dinh_quy(thang)}")
