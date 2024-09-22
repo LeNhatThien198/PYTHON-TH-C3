595d72bbd0d1b01ba0eb90b5f2352e9c5f6673eb
Lê Nhật Thiên <lenhatthien198@gmail.com>
2024-09-22 18:29:00 +0700

    DTH225771.Chuong03.Bai03.py

--- Python TH C3/chuong3/DTH225771.Chuong03.Bai03.py
+++ Python TH C3/chuong3/DTH225771.Chuong03.Bai03.py
@@ -1,4 +1,4 @@
-from math import sqrt
+import math
 print("Chương trình giải phương trình bậc 2")
 a=float(input("Nhập a: "))
 b=float(input("Nhập b: "))
@@ -19,8 +19,8 @@ else:
         x=-b/(2*a)
         print("Phương trình có nghiệm kép x1=x2= ",x)
     else:
-        x1=(-b-sqrt(delta))/(2*a)
-        x2=(-b+sqrt(delta))/(2*a)
+        x1=(-b-math.sqrt(delta))/(2*a)
+        x2=(-b+math.sqrt(delta))/(2*a)
         print("Phương trình có 2 nghiệm: ")
         print("x1= ", x1)
         print("x2= ", x2)
