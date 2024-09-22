6c0febb708c7ada05d684914d2b545496a42602c
Lê Nhật Thiên <lenhatthien198@gmail.com>
2024-09-22 18:52:15 +0700

    DTH225771.Chuong03.Bai07.py

--- Python TH C3/chuong3/DTH225771.Chuong03.Bai07.py
+++ Python TH C3/chuong3/DTH225771.Chuong03.Bai07.py
@@ -1,4 +1,13 @@
-''' Ta sẽ sử dụng hàm input '''
-Ví dụ
-s=input("Vui lòng nhập tên: ")
-print("Tên của bạn là: ",s)
+import datetime
+
+def ngay_ke_tiep(ngay, thang, nam):
+    ngay_hien_tai = datetime.date(nam, thang, ngay)
+    ngay_ke_tiep = ngay_hien_tai + datetime.timedelta(days=1)
+    return ngay_ke_tiep
+
+ngay = int(input("Nhập ngày: "))
+thang = int(input("Nhập tháng: "))
+nam = int(input("Nhập năm: "))
+
+ngay_sau = ngay_ke_tiep(ngay, thang, nam)
+print("Ngày kế tiếp là:", ngay_sau.strftime("%d/%m/%Y"))
