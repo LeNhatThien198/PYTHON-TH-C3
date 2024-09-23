dae050eeed2b918907ba774eafced4ccaafa72ec
Lê Nhật Thiên <lenhatthien198@gmail.com>
2024-09-23 20:36:43 +0700

    DTH225771.Chuong03.Bai11c1.py

--- Python TH C3/chuong3/DTH225771.Chuong03.Bai11.py
+++ Python TH C3/chuong3/DTH225771.Chuong03.Bai11.py
@@ -1,1 +1,36 @@
+while True:
+    try:
+        n=(input("Nhập vào n nguyên dương: "))
+        n=int(n)
+        if n <= 0:
+            print(n, "không phải là số nguyên dương! Vui lòng nhập lại.")
+            continue
+    except:
+        print(n,"không hợp lệ! Vui lòng nhập một số nguyên hợp lệ.")
+        continue
+   
+    dem = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            dem += 1
+    if dem==2:
+        print(n,"là số nguyên tố")
+    else:
+        print(n,"không phải số nguyên tố")
+    while True:
+        hoi=input("Bạn có muốn kiểm tra tiếp số nguyên tố không?(c/k): ").lower()
+        if hoi == "k":
+            break
+        elif hoi == "c":
+            break
+        else:
+            print(hoi, "là kí tự không hợp lệ! Vui lòng nhập lại.")
+        
+    if hoi == "k":
+        print("Tạm biệt người anh em!")
+        break
+
+
+
+    
 
