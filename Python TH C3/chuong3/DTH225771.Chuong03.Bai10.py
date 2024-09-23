5128f9dbf8a387193162a56d3301aba7296752a4
Lê Nhật Thiên <lenhatthien198@gmail.com>
2024-09-23 16:30:03 +0700

    DTH225771.Chuong03.Bai10.py

--- Python TH C3/chuong3/DTH225771.Chuong03.Bai10.py
+++ Python TH C3/chuong3/DTH225771.Chuong03.Bai10.py
@@ -1,6 +1,10 @@
-print("(a) x = x + 1 -> x += 1 ")
-print("(b) x = x / 2 -> x /= 2 ")
-print("(c) x = x - 1 -> x -= 1")
-print("(d) x = x + y -> x += y")
-print("(e) x = x - (y + 7) -> x -= (y + 7)")
-print("g: number_of_closed_cases = number_of_closed_cases + 2*ncc -> number_of_closed_cases += 2*ncc")
+x=int(input("Nhập vào x: "))
+n=int(input("Nhập vào N: "))
+s=0
+for i in range(1,n + 1):
+    tu=x**i
+    mau=1
+    for j in range(1,i + 1):
+        mau=mau*j
+    s=s+(tu/mau)
+print("s({},{})={}".format(x,n,s))
