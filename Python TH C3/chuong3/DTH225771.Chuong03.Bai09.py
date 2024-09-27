9f291292ace17a16252ca1ade1cd4ab1a3c2e346
Lê Nhật Thiên <lenhatthien198@gmail.com>
2024-09-27 16:21:41 +0700

    DTH225771.Chuong03.Bai09.py

--- Python TH C3/chuong3/DTH225771.Chuong03.Bai09.py
+++ Python TH C3/chuong3/DTH225771.Chuong03.Bai09.py
@@ -1,14 +1,17 @@
 def xac_dinh_quy(thang):
     if thang < 1 or thang > 12:
         return "Tháng {} không hợp lệ".format(thang)
-    if thang <= 3:
-        return "Tháng {} thuộc Quý 1".format(thang)
-    elif thang <= 6:
-        return "Tháng {} thuộc Quý 2".format(thang)
-    elif thang <= 9:
-        return "Tháng {} thuộc Quý 3".format(thang)
-    else:
-        return "Tháng {} thuộc Quý 4".format(thang)
+    quy = {
+        1: "Quý 1", 2: "Quý 1", 3: "Quý 1",
+        4: "Quý 2", 5: "Quý 2", 6: "Quý 2",
+        7: "Quý 3", 8: "Quý 3", 9: "Quý 3",
+        10: "Quý 4", 11: "Quý 4", 12: "Quý 4"
+    }
+    return "Tháng {} thuộc {}".format(thang, quy[thang])
 
-thang=int(input("Nhập một tháng (1-12): "))
-print(xac_dinh_quy(thang))
+def main():
+    thang = int(input("Nhập một tháng (1-12): "))
+    print(xac_dinh_quy(thang))
+    
+    
+main()
