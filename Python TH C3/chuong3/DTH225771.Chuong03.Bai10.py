3f5426225e4cede392c6c502e3de46920a5a7346
Lê Nhật Thiên <lenhatthien198@gmail.com>
2024-10-06 16:14:43 +0700

    DTH225771.Chuong03.Bai10.py

--- Python TH C3/chuong3/DTH225771.Chuong03.Bai10.py
+++ Python TH C3/chuong3/DTH225771.Chuong03.Bai10.py
@@ -4,7 +4,7 @@ s=0
 for i in range(1,n + 1):
     tu=x**i
     mau=1
-    for j in range(1,i + 1):
+    for j in range(1,i + 1): #lấy kết quả lần lặp cuối của i này xong mới chạy tiếp i trên
         mau=mau*j
     s=s+(tu/mau)
 print("s({},{})={}".format(x,n,s))
